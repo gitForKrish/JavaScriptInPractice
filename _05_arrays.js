99619e2d557423a9f6b7b958b8d01fe09c8b8bbe
Krishnendu Mandal <krishnendu.mandal89@gmail.com>
2019-09-07 20:58:48 +0530

    updated arrays

--- _05_arrays.js
+++ _05_arrays.js
@@ -11,14 +11,41 @@ a[10] = 77;
 console.log(a);             // [ 4, 8, 16, 42, 52, <5 empty items>, 77 ]
 console.log(a.length);      // 11
 
+// Add to the end of an Array
 a.push(80);
 console.log(a);             // [ 4, 8, 16, 42, 52, <5 empty items>, 77, 80 ]
+
+// Remove from the end of an Array
 a.pop();
 a.pop();
 a.pop();
 a.pop();
 a.pop();
 console.log(a);             // [ 4, 8, 16, 42, 52, <2 empty items> ]
+
+// remove element from the front
+a.shift();                  
+console.log(a);             // [ 8, 16, 42, 52, <2 empty items> ]
+
+// add element to the front 
+a.unshift(10);    
+console.log(a);             // [ 10, 8, 16, 42, 52, <2 empty items> ]
+
+// Find the index of an item in the Array
+console.log(a.indexOf(10)); // 0
+
+// Loop over an Array
+a.forEach(function(item, index){
+    console.log('Index: ' + index + ', Value: ' + item);
+});
+
+// Remove an item by index position
+a.splice(3,1);
+console.log(a);             // [ 10, 8, 16, 52, <2 empty items> ]
+
+// Copy an Array
+let shallowCopy = a.slice();
+console.log(shallowCopy);   //[ 10, 8, 16, 52, <2 empty items> ]
 
 let b = [12, 'Alex', true]; // array can hold different types of values
 console.log(b);             // [ 12, 'Alex', true ]
