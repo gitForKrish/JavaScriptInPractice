a5cc1712acb704c684cdb13b153ac6e9d5ffcb3c
Mandal <krishnendu.mandal@siemens.com>
2019-09-09 16:27:07 +0530

    Javascript datatypes

--- _03_datatypes.js
+++ _03_datatypes.js
@@ -13,10 +13,36 @@ console.log(typeof a); // undefined as type
 
 // variable doesn't have datatype, it is the value that it store, defined the datatype
 let changeDatatype = 10;
-console.log(typeof changeDatatype);
+console.log(typeof changeDatatype); // number
 changeDatatype = 'value is changed';
-console.log(typeof changeDatatype);
+console.log(typeof changeDatatype); // string
 changeDatatype = true;
-console.log(typeof changeDatatype);
+console.log(typeof changeDatatype); // boolean
 changeDatatype = undefined;
-console.log(typeof changeDatatype);
+console.log(typeof changeDatatype); // undefined
+
+let obj = {};
+console.log(typeof obj);    // object
+
+let arr = [];
+console.log(typeof arr);    // object
+
+let nothing = null;
+console.log(typeof nothing);    // object
+
+let car = {
+    make: 'bmw',
+    model: ['750li', '550Pi'],
+    price: 500000,
+    colors: [
+        {'750li': 'red'},
+        {'550Pi': 'white'}
+    ]
+};
+console.log(typeof car);        // object
+console.log(typeof car.make);   //  string
+console.log(typeof car.model);  // object
+console.log(typeof car.price);  // number
+console.log(typeof car.colors); // object
+console.log(typeof car.colors[0]);  // object
+console.log(typeof car.colors[0]['750li']); // string
