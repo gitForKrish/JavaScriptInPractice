9050d70f52d161b416348ca6c55773429d7b4a6d
Krishnendu Mandal <krishnendu.mandal89@gmail.com>
2019-09-12 08:51:37 +0530

    date type

---
+++ _24_date_type.js
@@ -0,0 +1,11 @@
+let today = new Date();
+console.log(today);
+
+let myDOB = new Date('May 05, 1989 08:20:20');
+
+let elapsedTime = today - myDOB;
+console.log(elapsedTime);
+
+// check in details
+
+
