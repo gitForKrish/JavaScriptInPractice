3fe6e8cebd57ebbc4a834272128719a7fa5b11e0
Krishnendu Mandal <krishnendu.mandal89@gmail.com>
2019-09-15 16:35:00 +0530

    math object

---
+++ ModernJavaScript/01_JS_Sandbox/03_MathObject.js
@@ -0,0 +1,22 @@
+let val;
+val = Math.PI;
+val = Math.E;
+
+val = Math.round(2.4);
+val = Math.round(2.8);
+
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+
+val = Math.sqrt(64);
+val = Math.pow(8, 2);
+
+val = Math.abs(-2.4);
+val = Math.min(8, 2,5,8,9,7,-1,-4);
+val = Math.max(8, 2,5,8,9,7,-1,-4);
+
+val = Math.random();
+val = Math.random() * 20; // Between 0.000 to 19.9999..
+val = Math.floor(Math.random() * 20 +1);  // between 1 to 20 (inclusive)
+
+console.log(val);
