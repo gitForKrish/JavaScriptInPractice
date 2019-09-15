4a3aab34885c3985aa5775a7b3a366517bee919f
Krishnendu Mandal <krishnendu.mandal89@gmail.com>
2019-09-15 12:58:27 +0530

    added Const

--- JavaScriptForAbsoluteBeginners/_02_variables.js
+++ JavaScriptForAbsoluteBeginners/_02_variables.js
@@ -10,8 +10,20 @@ let z = x + y; // + operator works as Addition
 console.log('Answer: ' + z); // + operator works as concatenation
 
 // You can not use keywords as identifier
-let let = 8; //SyntaxError: let is disallowed as a lexically bound name
+// let let = 8; //SyntaxError: let is disallowed as a lexically bound name
 
 // Use camel casing for name
 let firstNumber = 10;
 let secondNumber = 15;
+
+// using constant variable
+const name = 'Krish';
+console.log(name);
+
+// 1. We can not do reassignment
+// name = 'Tanmoy';  // TypeError: Assignment to constant variable.
+
+// 2. Only variable decalaration is restricted, you need to initialize also
+// const number; // SyntaxError: Missing initializer in const declaration
+// number = 100;
+// console.log(number);
