c16c8e6d0b72afb09ed807e4513f4b05d3b8487e
Krishnendu Mandal <krishnendu.mandal89@gmail.com>
2019-10-23 20:25:33 +0530

    Console and DOM API

---
+++ NotesForProfessional/Chapter 1 Getting started with JavaScript/01_Using_console.log.js
@@ -0,0 +1,27 @@
+console.log("Hello Console");
+
+// Logging variable
+let foo = "bar";
+console.log(foo);
+
+// Logging multiple variables
+let first = "first value",
+  second = "second value",
+  third = "third value";
+console.log("First:", first, "Second:", second, "Third:", third);
+
+// Placeholder
+let greet = "hell",
+  who = "Jack";
+console.log("%s %s", greet, who);
+
+// Objects
+var evenNumbers = [2, 4, 6, 8, 10];
+console.log(evenNumbers);
+
+var person = {
+  name: "Stanley",
+  age: 32,
+  country: "INDIA"
+};
+console.log(person);
